f0115e24304e80a243792527255d78c6dbe203c1
Steven Smith <smith84@llnl.gov>
2020-10-28 13:05:54 -0700

    Refactoring to make test work

--- test_MyClass.py
+++ test_MyClass.py
@@ -1,6 +1,6 @@
 # python -m unittest tests.TestMyClass
 import unittest
-from src.MyClass import MyClass
+from MyClass import MyClass
 
 class TestMyClass(unittest.TestCase):
 
@@ -8,7 +8,7 @@ class TestMyClass(unittest.TestCase):
         self.myclass = MyClass(7,13)
 
     def test_do_something(self):
-        self.assertEqual(self.myClass.do_something(),20)
+        self.assertEqual(self.myclass.do_something(),20)
 
 if __name__ == "__main__":
     unittest.main()
